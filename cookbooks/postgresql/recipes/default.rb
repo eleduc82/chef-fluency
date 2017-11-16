cd901e08982631cb741eec693fada19002f38165
Etienne Leduc <etienne.leduc82@gmail.com>
2017-11-15 19:30:23 -0500

    corrections to postgresql default recipe

--- cookbooks/postgresql/recipes/default.rb
+++ cookbooks/postgresql/recipes/default.rb
@@ -5,8 +5,8 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 
-package 'postgresql-serv' do
-	notifies :run, 'execute[postgresql-init'
+package 'postgresql-server' do
+	notifies :run, 'execute[postgresql-init]', :immediately
 end
 
 execute 'postgresql-init' do
@@ -14,6 +14,6 @@ execute 'postgresql-init' do
 	action :nothing
 end
 
-service 'postgrsql' do
+service 'postgresql' do
 	action [:enable, :start]
 end
